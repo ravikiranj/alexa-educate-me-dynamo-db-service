90779190a8f0d6275a1a0e4c2a7a066a21247be1
Ravikiran Janardhana <ravijan@amazon.com>
2018-03-28 10:09:42 -0700

    Added backup

--- src/main/java/com/alexa/educateme/models/TopicIdResponse.java
+++ src/main/java/com/alexa/educateme/models/TopicIdResponse.java
@@ -1,10 +1,14 @@
 package com.alexa.educateme.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TopicIdResponse {
     private final String topic;
     private final String id;
 
-    public TopicIdResponse(String topic, String id) {
+    @JsonCreator
+    public TopicIdResponse(@JsonProperty("topic") String topic, @JsonProperty("id") String id) {
         this.topic = topic;
         this.id = id;
     }
--- src/main/java/com/alexa/educateme/routes/GetTopicId.java
+++ src/main/java/com/alexa/educateme/routes/GetTopicId.java
@@ -2,23 +2,24 @@ package com.alexa.educateme.routes;
 
 import com.alexa.educateme.models.DynamoDbErrorResponse;
 import com.alexa.educateme.models.DynamoDbTopicResponse;
-import com.alexa.educateme.models.HelloWorldResponse;
 import com.alexa.educateme.models.TopicIdResponse;
 import com.alexa.educateme.util.DynamoDbHelper;
 import com.alexa.educateme.util.ObjectMapperHelper;
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.eclipse.jetty.http.HttpStatus;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -48,10 +49,41 @@ public class GetTopicId implements Route {
             response.status(HttpStatus.OK_200);
             return new TopicIdResponse(topic, dynamoDbTopicResponse.getItemMetaData().getTopicId());
         } else {
+            TopicIdResponse resp = getResponseFromWikipediaScraper(topic);
+            if (resp != null) {
+                return resp;
+            }
+
+            // We failed, just send 404
             String error = "Could not find topic details for " + topic;
             LOG.info(error);
             response.status(HttpStatus.NOT_FOUND_404);
             return new DynamoDbErrorResponse(error);
         }
+    }
+
+    private TopicIdResponse getResponseFromWikipediaScraper(String topic) {
+        try {
+            String url = String.format("http://ec2-54-218-93-249.us-west-2.compute.amazonaws.com/topic/%s/", topic);
+            LOG.info("Making an API call to " + url + ", since we didn't find it in dynamo db");
+            try (InputStream is = new URL(url).openStream()){
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                String jsonText = readAll(rd);
+                return ObjectMapperHelper.OBJECT_MAPPER.readValue(jsonText, TopicIdResponse.class);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOG.info("Encountered exception = " + e);
+            return null;
+        }
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
     }
 }
